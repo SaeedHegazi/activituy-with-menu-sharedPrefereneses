74cd14d9642f1e09896f9311949c24f5734e91c2
PC-17 <ahijazi083@gmail.com>
2025-09-10 08:57:36 +0300

    123

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,11 +11,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationBarItemView;
 
+public class MainActivity extends AppCompatActivity {
+private NavigationBarItemView nav;
+public Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        nav=findViewById(R.id.bottom_navigation);
         setContentView(R.layout.activity_main);
     }
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -25,7 +30,10 @@ public class MainActivity extends AppCompatActivity {
 }
 public boolean onOptionsItemSelected(MenuItem item){
         int id=item.getItemId();
-        if (id == R.id.home){}
+        if (id == R.id.home){
+            intent = new Intent(String.valueOf(MainActivity.this),MainActivity);
+
+        }
         else if (id == R.id.dash){}
         else if (id == R.id.login){}
 
@@ -34,4 +42,6 @@ public boolean onOptionsItemSelected(MenuItem item){
         return true;
 
     }
+
+
 }
